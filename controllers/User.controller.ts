6788a5b7dbbc61a1b72d87f4af47dbc62dc466e8
Gui <guitimliu@gmail.com>
2023-05-04 02:56:45 +0800

    feat: #7

--- controllers/User.controller.ts
+++ controllers/User.controller.ts
@@ -15,8 +15,58 @@ export const UserController = {
       errorHandler(res, e)
     }
   },
-  async update(req: Request, res: Response) {
+  async update(req: Request, res: Response) { 
     // 修改會員資料
+    try {
+      // 不顯示出來的欄位
+      const hideFields = {
+        'id': 0,
+        'password': 0,
+        'createTime': 0,
+        'updateTime': 0,
+      };
+
+      const payload = new User(req.body);
+
+      // 驗證資料
+      const validateError = payload.validateSync()
+      if (validateError) throw validateError
+
+      // 修改會員資料
+      await User.updateOne({
+        id: req.body.id,
+      }, {
+        data: req.body.data,
+        account: null,
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+        image: req.body.image,
+        isAllowedNotifications: req.body.isAllowedNotifications,
+        isSubscribed: req.body.isSubscribed,
+        customedUrl: req.body.customedUrl,
+        gender: req.body.gender,
+        birthday: req.body.birthday,
+        address: req.body.address,
+        website: req.body.website,
+        facebook: req.body.facebook,
+        instagram: req.body.instagram,
+        youtube: req.body.youtube,
+        updateTime: Date.now
+      }).catch(() => { throw '會員不存在' })
+
+      const result = await User.find({
+        'id': req.body.id
+      }, hideFields);
+
+      successHandler(res, {
+        "code": 200,
+        "message": "Success",
+        "data": result[0],
+      });
+    } catch(e) {
+      errorHandler(res, e)
+    }
   },
   options(req: Request, res: Response) {
     successHandler(res)
--- models/User.model.ts
+++ models/User.model.ts
@@ -7,8 +7,57 @@ const UserSchema = new Schema<IUser>(
     account: {
       type: String,
       required: [ true, '帳號必填' ]
-    }
-    // 繼續填寫
+    },
+    name: {
+      type: String,
+      required: [ true, '真實姓名必填' ]
+    },
+    username: {
+      type: String,
+      required: [ true, '用戶名稱必填' ]
+    },
+    email: {
+      type: String,
+      required: [ true, '信箱必填' ]
+    },
+    image: {
+      type: String,
+      default: null,
+    },
+    isAllowedNotifications: {
+      type: Boolean,
+      default: true,
+    },
+    isSubscribed: {
+      type: Boolean,
+      default: true,
+    },
+    customedUrl: {
+      type: String,
+      default: null,
+    },
+    gender: {
+      type: Number,
+      default: -1,
+    },
+    birthday: {
+      type: Number,
+    },
+    address: {
+      type: String,
+    },
+    website: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+    youtube: {
+      type: String,
+    },
   },
   {
     versionKey: false
